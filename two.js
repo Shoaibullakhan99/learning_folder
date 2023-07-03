53eb04d56a90c46c4f65f8fe01c68f18d7ccd148
SHoaibullakhan99 <shoaibullakhan1999@outlook.com>
2023-07-03 14:18:57 +0530

    initial commit

---
+++ two.js
@@ -0,0 +1,9 @@
+function threeSum(nums) {}
+
+/*
+  first unsorted array is required, then the length of array should be calculated,
+  then sort the array in ascending order, then loop through i elements, upto n - 2,
+  because there are 3 pair of elements, initialize j and k as i + 1 and n - 1 respectively 
+  and while j < k check if there sum is 0;
+  and continue to find the elements.....
+*/
